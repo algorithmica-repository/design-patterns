4f5b08d1aa2d79b7958f5c59295a3f96e6e9894e
Algorithmica <info@algorithmica.co.in>
2019-08-09 15:44:08 +0530

    Delete IState.java

--- 2019-aug/2.state-pattern/src/com/alg/oodesign/solution2/IState.java
+++
@@ -1,7 +0,0 @@
-package com.alg.oodesign.solution2;
-
-public interface IState {
-	void addGumballs(GumballMachine gbm, int count);
-	void insertCoin(GumballMachine gbm);
-	void turnHandle(GumballMachine gbm);
-}
